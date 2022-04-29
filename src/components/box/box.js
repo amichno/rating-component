d4471d48ba41ba42842c2f559eb1cff86a704ec8
amichno <95045327+amichno@users.noreply.github.com>
2022-04-29 10:01:40 +0200

    puch -A

--- src/components/box/box.js
+++ src/components/box/box.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Back = styled.div`
     background-color: black;
     width:100vw;
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     @media (max-width: 375px){
         width:100vw;
@@ -23,11 +26,38 @@ const Wrapper = styled.div`
     }
 `;
 
+const Wrapper = styled.div`
+    background-color:hsl(216, 12%, 8%);
+    border-radius: 5%;
+    @media (max-width: 375px) {
+        width: 90%;
+        height: 60%;
+        overflow-y: scroll;
+    }
+
+    @media (min-width:375px) and (max-width: 992px) {
+        width: 40%;
+        max-width: 450px;
+        height: 40%;
+        
+    }
+    @media (min-width:992px){
+        width: 30%;
+        max-width: 350px;
+        height: 50%;
+        max-height: 350px;
+
+    }
+
+`;
+
 const Box = () =>{
     return(
-        <div>
-            cokolwiek
-        </div>
+        <Back>
+            <Wrapper>
+
+            </Wrapper>
+        </Back>
     )
     
 }
